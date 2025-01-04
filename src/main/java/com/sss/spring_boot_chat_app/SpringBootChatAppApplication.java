678e30b1bd463cc6f605e8f556ee85342f22806b
Santhosh M <143023757+SanthoshM007@users.noreply.github.com>
2025-01-04 16:41:15 +0530

    Add files via upload

---
+++ src/main/java/com/sss/spring_boot_chat_app/SpringBootChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.sss.spring_boot_chat_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChatAppApplication.class, args);
+	}
+
+}
